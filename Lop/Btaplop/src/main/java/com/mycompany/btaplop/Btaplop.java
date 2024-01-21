23f575f3ec0d2ec6ff5b9af75b3ad250426bab7c
duongvietdungtn <duongvietdungtn@gmail.com>
2024-01-21 12:44:53 +0700

    1

---
+++ Lop/Btaplop/src/main/java/com/mycompany/btaplop/Btaplop.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.btaplop;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Dugdug
+ */
+public class Btaplop {
+
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Hello Dung");
+        f.setSize(400,300);
+        f.setVisible(true);
+    }
+}
